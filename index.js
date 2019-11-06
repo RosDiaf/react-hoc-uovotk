aa037198d2a9c2f90402740edb38c008a33c6016
RosDiaf <ros_diaf@hotmail.com>
2019-11-06 19:28:21 +0000

    Renders props examples added

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import { render } from 'react-dom';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 // -- Components
 import StockList from './Components/StockList';
 import UserList from './Components/UserList';
 import CompPage from './Components/Page';
 import Hoc from './Components/HOC';
+
+import Wrapper from './RenderProps/Wrapper';
 
 // -- Services
 import getPages from './Services/DrupalPageService'
@@ -37,11 +45,30 @@ const Page = Hoc(
 class App extends Component {
   render() {
     return (
-      <div>
-        {/*<Users></Users>
-        <Stocks></Stocks>*/}
-        <Page></Page>
-      </div>
+      <Router>
+        <Fragment>
+          <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+              <ul class="navbar-nav mr-auto">
+                <li class="nav-item">
+                  <Link to="/page" class="nav-link">HOC</Link>
+                </li>
+                <li class="nav-item">
+                  <Link to="/wrapper" class="nav-link">Render Props</Link>
+                </li>
+              </ul>
+            </div>
+          </nav>
+          <Switch>
+            <Route path="/page">
+              <Page />
+            </Route>
+            <Route path="/wrapper">
+              <Wrapper />
+            </Route>
+          </Switch>
+        </Fragment>
+      </Router>
     )
   }
 }
